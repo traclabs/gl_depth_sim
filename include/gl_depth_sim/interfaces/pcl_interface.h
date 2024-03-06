5ae8430a8bcdae1066f57f7d1932681fe1284cf3
Kenji Brameld <kbrameld@traclabs.com>
2024-03-06 20:11:17 +0000

    add template function for getting point cloud

--- include/gl_depth_sim/interfaces/pcl_interface.h
+++ include/gl_depth_sim/interfaces/pcl_interface.h
@@ -15,6 +15,40 @@ namespace gl_depth_sim
  */
 void toPointCloudXYZ(const CameraProperties& camera, const DepthImage& depth, pcl::PointCloud<pcl::PointXYZ>& out);
 
+/**
+ * @brief Projects the given depth image forward into space using the camera intrinsics from @e camera. The
+ * resulting point cloud is organized (has width and height) and NOT dense. Invalid points are marked as NAN.
+ */
+template <typename PointT>
+void toPointCloud(const gl_depth_sim::CameraProperties& camera, const gl_depth_sim::DepthImage& depth,
+                  pcl::PointCloud<PointT>& out)
+{
+  out.width = depth.cols;
+  out.height = depth.rows;
+  out.resize(out.width * out.height);
+  out.is_dense = false;
+
+  for (int i = 0; i < depth.rows; ++i)
+  {
+    for (int j = 0; j < depth.cols; ++j)
+    {
+      const float distance = depth.distance(i, j);
+      PointT& pt = out(j, i);
+
+      if (distance != 0.0f)
+      {
+        pt.z = distance;
+        pt.x = (j - camera.cx) * distance / camera.fx;
+        pt.y = (i - camera.cy) * distance / camera.fy;
+      }
+      else
+      {
+        pt.z = pt.x = pt.y = std::numeric_limits<float>::quiet_NaN();
+      }
+    }
+  } // end of loop
+}
+
 }
 
 #endif
